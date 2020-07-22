efddbbd55f94b2c87387d2f4b65f576534283108
limiao0606 <limiao0606@foxmail.com>
2020-07-22 04:09:34 -0700

    second

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,34 +1,53 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main()
+void CreatArr(vector<int> &vc,int n)
 {
-	int n = 0;
-	cin >> n;
-	vector<int> vc;
 	for (int i = 0;i < n;i++)
 	{
 		int a = 0;
 		cin >> a;
 		vc.push_back(a);
 	}
-	for (int i = 0;i < n-1;i++)
+}
+void Swap(int a, int b, int n)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+void BubbleSort(vector<int> &vc, int n)
+{
+	for (int i = 0;i < n - 1;i++)
 	{
 		for (int j = 0;j < n - i - 1;j++)
 		{
 			if (vc[j] > vc[j + 1])
 			{
-				int temp = vc[j];
-				vc[j] = vc[j + 1];
-				vc[j + 1] = temp;
+				swap(vc[j],vc[j+1]);
 			}
 		}
 	}
+}
+void Print(vector<int> &vc, int n)
+{
 	for (int i = 0;i < n;i++)
 	{
-		cout << vc[i]<<" ";
+		cout << vc[i] << " ";
 	}
+}
+int main()
+{
+	int n = 0;
+	cin >> n;
+	vector<int> vc;
+	CreatArr(vc, n);
+	cout << "ÅÅÐòÇ°£º" << endl;
+	Print(vc,n);
 	cout << endl;
+	BubbleSort(vc,n);
+	cout << "ÅÅÐòºó£º" << endl;
+	Print(vc, n);
 	system("pause");
 	return 0;
 }
